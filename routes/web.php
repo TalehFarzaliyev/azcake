c43e96de9e4f7fe411a356774c172ec90741d1ef
Rauf abbas <rafo.abbaS@gmail.com>
2021-01-25 10:55:25 +0300

    All updated

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| Rauf Abbas
 */
 Auth::routes();
+
 
 Route::get('', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('products', [\App\Http\Controllers\ProductController::class, 'index'])->name('products');
